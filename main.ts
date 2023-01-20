72cfdcf3d164fc2fcf9791ecee5e30873e625911
kuwayoshi0305 <123169372+kuwayoshi0305@users.noreply.github.com>
2023-01-20 21:01:47 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
